55965ce03d8f336826e9673807032e5e29191035
charlotte@virtualtrips.io <charlotte@virtualtrips.io>
2021-07-27 14:26:30 +0100

    ts setup

---
+++ useful notes/useful notes.js
@@ -0,0 +1,16 @@
+
+No JSX - use '.ts'
+
+Will the file contain a React component, or any kind of JSX? - use '.tsx'
+
+// Typescript will complain if don't use this rule
+
+
+// Passing props
+
+Two big checks by Typescript:
+
+1. Are we providing the correct props to Child when we show it in Parent?
+
+2. Are we using the correctly named + typed props in Child?
+
